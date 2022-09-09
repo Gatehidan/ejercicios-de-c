5b61d6a9a4fe736220cc4d2aea0a254de50219d5
INTEL <INTEL@DESKTOP-UV4VDA9>
2022-09-08 22:45:30 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio5
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+            int limite;
+            double cuadrado;
+            int numero = 2;
+            int cotador;
+            cotador = 0;
+            limite = 10;
+            for (cotador = 0; cotador <= limite; cotador++)
+            {
+                cuadrado = Math.Pow(numero, cotador);
+                Console.WriteLine($"El resultado del número 2 elevado en {cotador} es : {cuadrado}");
+            }
+
+        }
+    }
+}
